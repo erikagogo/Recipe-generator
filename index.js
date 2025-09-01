8262cecf90088026fa5386b3fe277f26c8a178c0
erikagogo <erika.gogo@gmail.com>
2025-09-01 14:00:45 -0400

    Added basic Javascript and added typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Make the dressing but leave in a separate small bowl...",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generateRecipe);
